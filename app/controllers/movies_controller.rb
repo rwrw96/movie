50fa5f7313ff7fd3815367baad17dacaf220003a
rwrw96 <kazuki137@gmail.com>
2021-04-10 23:38:31 +0000

    test search.html.erb 条件分岐delete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,28 @@
 class MoviesController < ApplicationController
-  require 'themoviedb-api'
-  Tmdb::Api.key("ご自身のAPI Key")
+  require "themoviedb-api"
+  require 'rubygems'
+  # require 'ruby-tmdb'
+  Tmdb::Api.key("f666fc01f99324fbc4beea6ae1f0e427")
   Tmdb::Api.language("ja")
+#   # setup your API key
+# Tmdb.api_key = "t478f8de5776c799de5a"
+
+# # setup your default language
+# Tmdb.default_language = "en"
+
+# @movie = TmdbMovie.find(:title => "Iron Man", :limit => 1)
+# # => <OpenStruct>
+
+# @movie.name
+# # => "Iron Man"
+
+# @movie.posters.length
+# # => 12
+
+# @movie.posters.first.data
+# # => [binary blob representing JPEG]
+  
+  
   def search
   end
 
