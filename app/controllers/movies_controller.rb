653039a461715c92a84c60fc7c411bd3f9e7161b
rwrw96 <kazuki137@gmail.com>
2021-04-11 00:02:17 +0000

    API_key include

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,7 @@
 class MoviesController < ApplicationController
   require "themoviedb-api"
-  require 'rubygems'
+  # require 'rubygems'
   # require 'ruby-tmdb'
-  Tmdb::Api.key("f666fc01f99324fbc4beea6ae1f0e427")
   Tmdb::Api.language("ja")
 #   # setup your API key
 # Tmdb.api_key = "t478f8de5776c799de5a"
